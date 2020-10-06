4e5262847c72322da0d78ef37e73c519e8943c84
JacksonWakefield <55509800+JacksonWakefield@users.noreply.github.com>
2020-10-05 19:39:39 -0700

    second commit

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,35 @@
 package cse360assignment02;
 
+
 public class AddingMachine {
   private int total;
+  private String memory;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    memory = "0 ";
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+	  total += value;
+	  memory += "+ " + value;
   }
 
   public void subtract (int value) {
+	  total -= value;
+	  memory += "- " + value;
   }
 
   public String toString () {
-    return "";
+    return memory;
   }
 
   public void clear() {
+	  total = 0;
+	  memory = "0 ";
   }
 }
